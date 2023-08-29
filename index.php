8cdbd895eff0f764886d062f7d11fc6eeaeab4fe
Emmanuel Blin <76560730+emmanuel-blin@users.noreply.github.com>
2023-08-29 11:05:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: emmanuel-blin/tarte-au-citron-js
+ * GitHub Branch: main
+ * Text Domain: tarte-au-citron-rgpd
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
